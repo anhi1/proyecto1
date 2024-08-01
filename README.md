38ad4a232935a38b5fddd631d6f22bb429226ac8
anhi1 <anhiemolina@gmail.com>
2024-08-01 18:38:26 +0200

    tailwind

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 - instalar node que se compatilble por ejemplo v20.16.01
 - npm install -g @angular/cli@17.3.8
 - verificar version: ng version
+  
+# Modulos (EN ANGULAR 17 YA NO HAY MODULOS)
+- ng generate module nombre-del-modulo
+
+# Componente
+- ng generate componente nombre-del-compnet
+
+# Instalar TAILWIND
+- npm install -D tailwindcss postcss autoprefixer
+- npx tailwindcss init
+- 
+ # Pegar en styles css 
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto1';
+  title = 'Annie';
+  subtitle = 'es lo primero que escribi';
 }
